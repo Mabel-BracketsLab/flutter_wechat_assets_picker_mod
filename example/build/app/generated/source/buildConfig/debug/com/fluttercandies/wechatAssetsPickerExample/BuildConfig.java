7d41256a3692ac3b0786b69f9f8c8c96694e585b
Mabel <mabel@bracketslab.com>
2023-05-31 09:33:27 +0200

    First commit with changes in video builder

---
+++ example/build/app/generated/source/buildConfig/debug/com/fluttercandies/wechatAssetsPickerExample/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.fluttercandies.wechatAssetsPickerExample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.fluttercandies.wechatAssetsPickerExample";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 37;
+  public static final String VERSION_NAME = "8.4.3";
+}
